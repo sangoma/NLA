aac49ba34e7a3fa386395cf3f02204ff0bde3597
Tyler Goodlet <tgoodlet@gmail.com>
2013-04-17 03:10:25 -0400

    all data model stuctures are now in callset.py

--- callset.py
+++ callset.py
@@ -56,7 +56,7 @@ class CallSetFactory(object):
         cs = CallSet("super", subset_field_tag)
     #TODO: consider attaching the factory to the callset itself?
         cs.factory = self
-        add_package(cs, log_package)
+        add_package_to_callset(cs, log_package)
 
         # allocate subsets and attach as attributes of the callset
         for s in cs._subset_tags.keys():
@@ -118,7 +118,7 @@ class CallSet(object):
         self._dup_dest = 0
         self._entries.clear()
         self._destinations.clear()
-        add_package(self, self._logs_pack)
+        add_package_to_callset(self, self._logs_pack)
         self.grapher.reset_cache()
 
     def select(self, index_itr):
@@ -151,7 +151,7 @@ class CallSet(object):
         # subset = factory.subset(self, 
         pass
 
-    def stats(self, field=None):
+    def summary(self, field=None):
         if field == None and type(self) == CallSet:
 
             #TODO: abstract this such that we cycle through all the subsets of this callset instead of a silly dict?
@@ -184,15 +184,18 @@ class CallSet(object):
                 # grapher_index = self.grapher.add(cl.wav)
                 # axes = self.grapher.plot(grapher_index)
 
-        # TODO: change the grapher.WavPack interface to be less index-deuschy (and avoid all this malarky)
-        cl_dicts = dict((index, cl) for index,cl in zip(self.grapher.add(wav_list), call_logs))
-        axes_dicts = self.grapher._plot(cl_dicts.keys()) #[index for index in cl_dicts.keys()])
+        if wav_list:
+            # TODO: change the grapher.WavPack interface to be less index-deuschy (and avoid all this malarky)
+            cl_dicts = dict((index, cl) for index,cl in zip(self.grapher.add(wav_list), call_logs))
+            axes_dicts = self.grapher._plot(cl_dicts.keys()) #[index for index in cl_dicts.keys()])
 
-        for ax, cl in zip(axes_dicts.values(), cl_dicts.values()):
-            # parse .log files for prob computations
-            grapher.red_vline(ax, cl.audio_connect_time, label='SIP 200 OK')
+            for ax, cl in zip(axes_dicts.values(), cl_dicts.values()):
+                # parse .log files for prob computations
+                grapher.red_vline(ax, cl.audio_connect_time, label='SIP 200 OK')
+        else:
+            print("\nsorry can't plot calls with no audio...")
 
-    def close_fig(self):
+    def close_figure(self):
         self.grapher.close_all_figs
 
     @property
@@ -225,7 +228,7 @@ class CallSet(object):
     #     pass
 
 
-def add_package(callset, logs_package):
+def add_package_to_callset(callset, logs_package):
     '''populate a callset from a logs package'''
 
     # if callset is not populated with data, gather template info
@@ -289,7 +292,7 @@ def printer(obj=None, fields=[], field_width=20, delim='|'):
     '''printing closure: use for printing tables (list of lists).
     Use this to create a printer for displaying CallSet content
     NOTE: should only be passed a callset AFTER the callset set has been populated
-    with data (run callset.add_package first)'''
+    with data (run callset.add_package_to_callset first)'''
 
     if type(obj) == CallSet:
         #TODO: but how will these update dynamically???
@@ -351,3 +354,324 @@ def parse_log_file(logpaths_obj):
     return None
 def ring_in_precon(audiofile):
     return None
+
+# #######################
+# this WAS the nla.py stuff #
+# #######################
+
+# TODO:
+# -implement stats computation -> should make this the 'summary' property
+# -implement xml writer for lin_log_set
+# -create a path index to quickly parse once nla front end has converted a package
+# -implement signalling parser -> check travis' code
+# DONE - implement wav file plotter
+# -check for ipython and boot if available, else print stats and gen packages?
+# -ask user if they would like to verify path index
+# - front end script to parse xmls and just spit out the disposition
+#   values (load into db?) if there is no csv to reference
+# - func which takes in a text file listing cids -> creates a subset
+
+# Ideas
+# done - create a seperate class CallLogs package
+# - os.walk instead of 'find' utility
+
+import sys, os, getopt, subprocess
+import xml.etree.ElementTree as ET
+import csv
+import shutil
+import os.path as path
+
+# important field strings
+cid_f          = 'Netborder Call-id'
+phone_num      = 'Phone Number'
+nca_result     = 'NCA Engine Result'
+det_nca_result = 'Detailed Cpd Result'
+
+# to remove in the wav files for sa package
+troublesome_suffix = '.analyzer-engine.0.0'
+
+# 'prepped' package names
+stats_anal_package = "./sa_package"
+tuning_dir         = "./tuning_logs_package"
+
+# if this file exists the audio data has already been prepped
+# this index allows for fast file path loading without having to re-scan for files
+log_index_f_name = "log-index.xml"
+
+
+# create a dir if it doesn't already exist
+def verbose_make_dir(d):
+    if not os.path.exists(d):
+        print("-> creating package dir: " + d)
+        os.makedirs(d)
+        return True
+    else:
+        print("WARNING : ", d, " already exists...overwriting\n")
+        return False
+
+def scan_logs(re_literal, search_dir, method='find'):
+    if method == 'find':
+        try:
+            found = subprocess.check_output(["find", search_dir, "-regex", "^.*" + re_literal + ".*"])
+            paths = found.splitlines()
+
+            # if the returned values are 'bytes' then convert to strings ie utf-8
+            str_paths = [b.decode() for b in paths]
+            return str_paths
+
+        except subprocess.CalledProcessError as e:
+            print("scanning logs failed with output: " + e.output)
+
+    elif method == 'walk':
+        #TODO: os.walk method
+        print("this should normally do an os.walk")
+    else:
+        print("no other logs scanning method currentlyl exists...sorry")
+
+def write_log_index_f_name(cid, logs_list):
+    # TODO: write a log-index xml file into the lin_log_set dir for
+    # quick parsing on reload???
+    pass
+
+def xml_log_update(log_obj):
+    if log_obj.xml is None:
+        print("WARNING: the log set for '", log_obj.cid, "' does not seem to contain an xml file\n"
+              "WARNING: unable to parse XML file in logs!\n")
+    else:
+        f    = log_obj.xml
+        tree = ET.parse(f)
+        root = tree.getroot()
+
+        # get the important children elements
+        cdr = root.find("./CallDetailRecord")
+        ci  = cdr.find("./CallInfo")
+
+        # parse the numeric CallInfo
+        log_obj.audio_time         = float(ci.find("./TimeFirstAudioPushed").text)
+        log_obj.connect_time       = float(ci.find("./TimeConnect").text)
+        log_obj.modechange_time    = float(ci.find("./TimeModeChange").text)
+        log_obj.audio_connect_time = log_obj.connect_time - log_obj.audio_time
+
+        # parse the rest of the CDR
+        log_obj.cpa_result = cdr.find("./CPAResult").text
+        log_obj.final_prob = cdr.find("./CPAResultProbability").text
+
+def add_to_dir_package(log_list, dest_dir, output_format='same', remove_str=None):
+
+    wavs         = []
+    new_paths    = []
+    combine_flag = []
+    format_spec  = []
+
+    for path in log_list:
+        filename, extension = os.path.splitext(path)
+
+        # if extension == b'.wav':
+        if extension == '.wav':
+            # wavs.append(path.decode())
+            wavs.append(path)
+        else:
+            # new_paths.append(shutil.copy(path.decode(), dest_dir))
+            new_paths.append(shutil.copy(path, dest_dir))
+
+    # process the wave files
+    if len(wavs) > 1:
+        wavs.sort()         # sort in place
+        print("INFO: concatenating ->", " + ".join(os.path.basename(w) for w in wavs))
+        combine_flag = ["--combine", "concatenate"]
+
+    elif not wavs:
+        # if no wav file
+        return new_paths
+
+    wavname = os.path.basename(wavs[0])
+
+    if remove_str is not None:
+        # remove str part from file name
+        wavname.replace(remove_str, "")
+
+    wav_path = '/'.join([dest_dir, wavname])
+    new_paths.append(wav_path)
+
+    # call sox to do the conversion TODO: check sox exists at the front end?
+    if output_format == 'linear':
+        format_spec = ["-b", "16", "-e", "signed"]
+
+    retcode = subprocess.call(["sox"] + combine_flag + wavs + format_spec + [wav_path])
+    return new_paths
+
+class LogPackage(object):
+    '''A LogPackage is the in-memory representation of a customer provided log set.
+    More specifically, a LogSet references the actual data provided by the customer after
+    the call recording data has been converted to linear format.
+
+    A LogSet instance contains the following reference information:
+    - file paths to log files which are copied to a new directory which of 'prepped' audio files in lpcm format
+    - information parsed from the cdr/.xml
+    - counters of indexing errors between the cpa-stats.csv summary and the actual log files provided'''
+
+    def __init__(self, csv_file, logs_dir):
+
+        print("creating new logs package in memory...")
+        # self._id = callset_id
+        self.fields         = {}
+        self._field_mask    = []
+        self.mask_indices   = []
+
+        self.cid_unfound    = {}
+        self.call_logs      = {}
+        self.entries        = []
+        self.failed_entries = []
+
+        # compile data
+        self.load_logs(csv_file, logs_dir)
+
+    @property
+    def length(self):
+        return len(self.entries)
+
+    def load_logs(self, csv_file, logs_dir):
+        """ load a new log package into memory """
+
+        # default config
+        gen_lin = gen_sa = True
+
+        # check if log directory contains a log-index.xml file
+        index_file = os.path.join(logs_dir, log_index_f_name)
+        if os.path.isfile(index_file):
+            print("detected log index file : '", index_file, "'")
+
+            # a hack for now until I get time to pick a format for the index xml file
+            with open(index_file) as f:
+                if f.read() == '':
+                    print(index_file, "contains nothing...so let's not generate new packages")
+                    gen_lin = False
+                    gen_sa = False
+
+            # TODO: parse some kind of xml db file
+            print("\nCURRENTLY NOT IMPLEMENTED: should parse xml file here and populate the instance that way!")
+            # for list in xmlelement: (here xmelement is implemented by a generator)
+            #     blah = CallLogs(list)
+            pass
+
+        else:
+            print("no '", log_index_f_name, "' found!\n")
+
+            # create package dirs
+            verbose_make_dir(stats_anal_package)
+            verbose_make_dir(tuning_dir)
+
+            # create a log index file
+            print("creating a log index file...")
+            with open(index_file, 'a') as xml:
+                xml.write("")
+
+            print("scanning for log files ...")
+                  # "would you like to re-scan for log files on the system? [Y/n]")
+            # answer = raw_input()
+            # if answer == 'Y' or answer == '\n':
+                # pass
+            # else:
+                # print("exiting...")
+                # sys.exit(0)
+        try:
+            # load csv file and populate useful properties
+            print("opening csv file : '", csv_file, "'")
+            with open(csv_file) as csv_buffer:
+
+                # TODO: add a csv sniffer here to determine an excell dialect?
+                csv_reader = csv.reader(csv_buffer)
+
+                # if LogPackage is not populated with data, gather template info
+                if len(self.entries) == 0:
+                    self.title        = next(csv_reader)    # first line should be the title
+                    self.fields       = next(csv_reader)   # second line should be the field names
+                    self.field_widths = [0 for i in self.fields]
+
+                    # gather user friendly fields (i.e. fields worth reading on the CLI)
+                    self.cid_index           = self.fields.index(cid_f)
+                    self.phone_index         = self.fields.index(phone_num)
+                    self.result_index        = self.fields.index(nca_result)
+                    self.detail_result_index = self.fields.index(det_nca_result)
+
+                    # make a field mask for displaying a selection of fields
+                    self.mask_indices = [self.cid_index, self.result_index, self.detail_result_index]
+
+                    # create a list of indices
+                    self.width = len(self.fields)
+
+                # compile a list of csv/call entries
+                print("compiling log index...\n")
+                for entry in csv_reader:
+
+                    # search for log files in the file system using call-id field
+                    cid = entry[self.cid_index]
+                    log_list = scan_logs(cid, logs_dir)
+                    log_list = [path.abspath(l) for l in log_list]
+
+                    if len(log_list) == 0:
+                        print("WARNING : no log files found for cid :", cid)
+                        self.cid_unfound[cid] = 1
+                        next
+
+                    else:
+                        # copy to the stats analyser package dir
+                        if gen_sa == True:
+                            add_to_dir_package(log_list, stats_anal_package, remove_str=troublesome_suffix)
+
+                        # copy to the tuning package dir
+                        if gen_lin == True:
+                            log_list = add_to_dir_package(log_list, tuning_dir, output_format='linear')
+
+                        self.call_logs[cid] = CallLogs(cid, log_list)
+
+                    # TODO: use the "collections" module here?!
+                    # keep track of max str lengths for each field
+                    for i, column in enumerate(entry):
+                        if len(column) > self.field_widths[i]:
+                            self.field_widths[i] = len(column)
+
+                    # if all else is good add the entry to our db
+                    self.entries.append(entry)
+
+            print(str(sum(self.cid_unfound.values())), "cids which did not have logs in the provided package")
+
+        except csv.Error as err:
+            print('file %s, line %d: %s' % (csv_buffer, self._reader.line_num, err))
+            print("Error:", exc)
+            sys.exit(1)
+
+class CallLogs(object):
+    def __init__(self, cid, logs_list):
+        self.cid = cid
+        self.logs = []
+        self.xml = None
+
+        # list of wavs
+        wavs = []
+
+        for path in logs_list:
+            # assign properties by extension (note the 'byte' format)
+            filename, extension = os.path.splitext(path)
+            if extension == ".log":
+                self.logs.append(path)
+
+            elif extension == ".xml":
+                self.xml = path
+
+            elif extension == '.wav':
+                wavs.append(path)
+
+        if len(wavs) == 0:
+            print("WARNING : no wave files found for cid '", self.cid, "'")
+            self.wav = None
+
+        elif len(wavs) > 1:
+            print("Log instance was passed more then 1 wave file!\n"
+                  "WARNING: This probably means the wav files were not processed properly!")
+            wavs.sort()
+            self.wav = wavs[0]
+        else:
+            self.wav = wavs[0]
+
+        xml_log_update(self)
