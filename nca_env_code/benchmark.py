a91176baf3b3b527d5f85cee526624b53ff55d88
Tyler Goodlet <tgoodlet@gmail.com>
2013-04-19 15:52:09 -0400

    benchmark.py: add options argument to toggle between csv vs. .txt output

--- nca_env_code/benchmark.py
+++ nca_env_code/benchmark.py
@@ -10,11 +10,17 @@ benchmark_tool = os.path.expandvars("$PARAXIP/bin/netborder_x86_64-linux_gcc34/n
 
 config = "./Benchmark.properties"
 test_list = "test.audioset"
+
 output_file = "results.txt"
+op_csv_file = "results.csv"
+
+# choose which option : csv or text file output
+options = ['--output-csv', op_csv_file]
+#options = ['--print']
 
 if not os.path.exists(config):
-    print("E: no configuration file detected!?" + config)
-    sys.exit(1)
+	print("E: no configuration file detected!?" + config)
+	sys.exit(1)
 
 human_th_range = "[0.74,0.78]"
 machine_th_range = "[0.50,0.82]"
@@ -32,27 +38,27 @@ elif len(argv) > 2:
 else:
 	package_dir = os.path.abspath(argv[1])
 
-    print("re-writing" + test_list + " with absolute paths...\n")
-    paths = []
-    t = open(os.path.join(package_dir, test_list), 'r+')
-    paths = map(os.path.abspath, t.readlines())
-    t.close()
+	print("re-writing" + test_list + " with absolute paths...\n")
+	paths = []
+	t = open(os.path.join(package_dir, test_list), 'r+')
+	paths = map(os.path.abspath, t.readlines())
+	t.close()
 
-    t = open(os.path.join(package_dir, test_list), 'w+')
-    t.writelines(paths)
-    t.close()
+	t = open(os.path.join(package_dir, test_list), 'w+')
+	t.writelines(paths)
+	t.close()
 
-    print("beggining benchmark of data in '" + package_dir + "'")
+	print("beggining benchmark of data in '" + package_dir + "'")
 
 #command = [benchmark_tool]
 #command.append(options[:])
 #options.insert(0, benchmark_tool)
 
-    test_list = os.path.join(package_dir, test_list)
-    print("parsing benchmarking data set listed in '" + test_list + "'")
+	test_list = os.path.join(package_dir, test_list)
+	print("parsing benchmarking data set listed in '" + test_list + "'")
 
 # use the default options list
-    command = 	[benchmark_tool,
+	command = 	[benchmark_tool,
 				"--global-config", 
 				config, 
 				"--input-filename",
@@ -66,14 +72,13 @@ else:
 				"--global-threshold", 
 				global_th,
 				"--delay-type",
-				"START_OF_FILE", 
-				"--print"]
+				"START_OF_FILE"]
 
-    print("benchmarking...")
-    output = Popen(command, stdout=PIPE).communicate()[0]
-    print("finished benchmark processing.")
+	print("benchmarking...")
+	output = Popen(command + options, stdout=PIPE).communicate()[0]
+	print("finished benchmark processing.")
 
-    print("writing results to => " + output_file)
-    f = open(output_file, 'w')
-    f.write(output)
-    f.close
+	print("writing results to => " + output_file)
+	f = open(output_file, 'w')
+	f.write(output)
+	f.close
